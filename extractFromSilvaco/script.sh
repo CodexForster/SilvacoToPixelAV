cad6093042635d0c7d0e5cf600964dbbb0cf6352
CodexForster <dsheka3@uic.edu>
2024-04-22 19:59:36 -0500

    Updated codes

---
+++ extractFromSilvaco/script.sh
@@ -0,0 +1,88 @@
+# ========================
+# ELECTRIC FIELD GEN - FST
+# ========================
+python3 extract-2D.py --template template_E_Field_Z.set --set  cutZ_ --TwoDname map2Dz_ --ThreeDname ../cmsPixel_postBias_-100V.str --zmin 0 --zmax 6.25 --step 0.1 > allCuts.txt
+source allCuts.txt
+# Edit loop_Ei.in depending on the number of cuts
+deckbuild -run loop_Ex.in -outfile loop_Ex.out &
+deckbuild -run loop_Ey.in -outfile loop_Ey.out &
+deckbuild -run loop_Ez.in -outfile loop_Ez.out &
+python3 create-3D-map.py --prefix map2Dz_ --suffix _EFieldX.dat --outputname E_FieldX.dat --zmin 0 --zmax 6.25 --step 0.1
+python3 create-3D-map.py --prefix map2Dz_ --suffix _EFieldY.dat --outputname E_FieldY.dat --zmin 0 --zmax 6.25 --step 0.1
+python3 create-3D-map.py --prefix map2Dz_ --suffix _EFieldZ.dat --outputname E_FieldZ.dat --zmin 0 --zmax 6.25 --step 0.1
+# important to pass inputs in this order
+python merge_maps.py --output EField_YX.txt --input E_FieldX.dat E_FieldY.dat E_FieldZ.dat
+
+python3 extract-2D.py --template template_E_Field_X.set --set  cutX_ --TwoDname map2Dx_ --ThreeDname ../cmsPixel_postBias_-100V.str --zmin 0 --zmax 25 --step 0.5 > allCuts.txt
+source allCuts.txt
+# Edit loop_Ei.in depending on the number of cuts
+deckbuild -run loop_Ex.in -outfile loop_Ex.out &
+deckbuild -run loop_Ey.in -outfile loop_Ey.out &
+deckbuild -run loop_Ez.in -outfile loop_Ez.out &
+python3 create-3D-map.py --prefix map2Dx_ --suffix _EFieldX.dat --outputname E_FieldX.dat --zmin 0 --zmax 25 --step 0.5
+python3 create-3D-map.py --prefix map2Dx_ --suffix _EFieldY.dat --outputname E_FieldY.dat --zmin 0 --zmax 25 --step 0.5
+python3 create-3D-map.py --prefix map2Dx_ --suffix _EFieldZ.dat --outputname E_FieldZ.dat --zmin 0 --zmax 25 --step 0.5
+# important to pass inputs in this order
+python merge_maps.py --output EField_YZ.txt --input E_FieldX.dat E_FieldY.dat E_FieldZ.dat
+
+# ========================
+# WEIGHTING POTENTIAL GEN - FST
+# ========================
+python3 extract-2D.py --template template_E_Field_Z.set --set  cutZ_ --TwoDname map2Dz_ --ThreeDname ../cmsPixel_postBias_1V.str --zmin 0 --zmax 31.25 --step 1 > allCuts.txt
+source allCuts.txt
+deckbuild -run loop_Ex.in -outfile loop_Ex.out &
+python3 create-3D-map.py --prefix map2Dz_ --suffix _Potential.dat --outputname E_FieldX.dat --zmin 0 --zmax 31.25 --step 1
+
+# ========================
+# INTERPOLATION - LOCAL
+# ========================
+# export is optional
+export PATH=$PATH:/Users/danush/Documents/PixelAV/silvaco_datagen/recipes_c-ansi/lib/
+# gen efield
+# EField_YX.txt and EField_YZ.txt are generated from the prev steps and should be stored in prodname folder
+python3 gen_gridAndFieldFile.py --prodname silvaco50x13
+gcc gen_efield.c -o gen_efield -I ./recipes_c-ansi/include/ -I ./recipes_c-ansi/lib/ -I ./recipes_c-ansi/recipes/ ./recipes_c-ansi/lib/librecipes_c.a -lm
+./gen_efield silvaco50x13 100
+
+# example output for gen_efield
+
+# danush@danush silvaco_datagen % ./gen_efield silvaco50x13 100
+# Grid file = silvaco50x13/silvaco50x13_msh.grd, dessis plot file = silvaco50x13/silvaco50x13_100_des.dat
+# number of vertices = 47652
+# detector dimensions = 25.000000 6.250000 100.000000 um 
+# field = 441.889000 433.236000 -16.666500 V/cm 
+# enter the number of output grid points in each dim: nx[21], ny[21], nz[92]
+# 26 13 51
+# (nx, ny, nz) = (26, 13, 51)
+# total Lorentz Drift = 0.000000, average efield = nan 
+# enter zmin (E = 0 for z>zmin) 
+# 100
+# zmin = 100.000000 um
+
+# gen wpot
+# Potential_YX.txt is generated from the prev step and should be stored in prodname folder
+python3 gen_wgtpotGridAndFieldFile.py --prodname silvaco50x13wgt
+gcc gen_wpot.c -o gen_wpot -I ./recipes_c-ansi/include/ -I ./recipes_c-ansi/lib/ -I ./recipes_c-ansi/recipes/ ./recipes_c-ansi/lib/librecipes_c.a -lm
+./gen_wpot silvaco50x13wgt 1
+
+# example output for gen_wpot
+
+# danush@danush silvaco_datagen % ./gen_wpot silvaco50x13wgt 1
+# Grid file = silvaco50x13wgt/silvaco50x13wgt_msh.grd, dessis plot file = silvaco50x13wgt/silvaco50x13wgt_1_des.dat
+# number of vertices = 15444
+# first node = 0.000000, last node = 0.000000 
+# detector dimensions = 125.000000 31.250000 2.000000 um 
+# enter the number of 1/2 pixels in each direction [5]
+# 5
+# enter the number of output grid points in each dim: nx[21], ny[21], nz[92]
+# 26 13 51
+# (nx, ny, nz) = (26, 13, 51)
+# enter the x y coordinates and tolerance for plot axis
+# 2 2 0.1
+
+# ========================
+# PIXELAV DATA GEN - LPC
+# ========================
+gcc -c ppixelav2_list_trkpy_n_2f.c
+gcc -o pixelavrun ppixelav2_list_trkpy_n_2f.o -lm
+./pixelavrun 
--- extractFromSilvaco/zy_extract/extract-2D.py
+++ extractFromSilvaco/zy_extract/extract-2D.py
@@ -13,20 +13,12 @@ def argumentParser(arguments):
   parser.add_argument("--ThreeDname",help="3d map input file name",required=True)
   parser.add_argument("--zmin",help="zmin",type=float,required=True)
   parser.add_argument("--zmax",help="zmax",type=float,required=True)
-  parser.add_argument("--step",help="step",type=float,required=True)
-
-
+  parser.add_argument("--step",help="step size",type=float,required=True)
   args = parser.parse_args(arguments)
-
-
   return args
-
-
-
 
 if (__name__ == "__main__" ):
   args = argumentParser(sys.argv[1:])
-  #print(args.TwoDname)
 
   # we now write a series of cut files, one per z value between zmin and zmax
   # the program will also print out a series of commands for tonyplot3d to extract the 2d maps
@@ -44,61 +36,59 @@ if (__name__ == "__main__" ):
   map3Dname = args.ThreeDname
   zmin = args.zmin
   zmax = args.zmax
-  stepZ = args.step
+  stepz = args.step
 
   tmpzmin = int(zmin*100)
   tmpzmax = int(zmax*100)
-  tmpstepZ= int(stepZ*100)
+  tmpstepZ=int(stepz*100)
 
   # auxiliary variables for ELEV calculation
   ymin = 1
   ymax = -1
-
   # preparing the different components of the filenames to be handled
 
   (dirName, fileName) = os.path.split(templateFileName)
   (fileBaseName, fileExtension)=os.path.splitext(fileName)
-  #print (dirName)   
-  #print (fileName)  
-  #print (fileBaseName)    
-  #print (fileExtension) 
 
   # keywords to be searched in the set template file
-  pointHeader = "point_" 
+  pointHeader1 = "point_1"
+  pointHeader2 = "point_2"
+  pointHeader3 = "point_3"
   ELEVHeader = "ELEV"
 
   # open set template file for reading
-
   with open(templateFileName) as fin:
     inputLines = fin.readlines()
-  #print(inputLines)
 
   # printing the command used to run the program
   print("#File produced using the following command:")
   print(("#python %s %s") % (sys.argv[0],(' '.join(f'--{k} {v}' for k, v in vars(args).items()))))
 
-  # loop over all the z values to write out all the cut files and print the commands to run 
-  # the projections to planes at fixed z value
-
+  # loop over all the x values to write out all the cut files and print the commands to run 
+  # the projections to planes at fixed x value
   for iter, tmpz in enumerate(range(tmpzmin,tmpzmax+tmpstepZ, tmpstepZ)):
     # set output file name
-    z = round(tmpz/100,2)
-    #setFileName = baseSetFileName + str(iter) + fileExtension
+    if(tmpz>tmpzmax):
+      z = round(tmpzmax/100,2)
+    else:
+      z = round(tmpz/100,2)
     integer_part, decimal_part = str(z).split('.')
     tempstr = integer_part+'P'+decimal_part
     setFileName = baseSetFileName + str(iter) + fileExtension
     # set output file name
-    #print(setFileName)
     # open the set file in write mode
     with open(setFileName,'w') as fout:
       # read each line of the set template file
       for inputLine in inputLines:
         # look for keywords:
         # first is point_ header
-        if (re.search(pointHeader,inputLine)):
-          #print(("%s found in %s") % (pointHeader,inputLine))
-          # write the point_ position with the z coordinate
-          fout.write(("%s %.2f\n") % (inputLine.rstrip(),z))
+        # write the point_ position with the X coordinate
+        if (re.search(pointHeader1,inputLine)):
+          fout.write(("%s %.2f 0 0\n") % (inputLine.rstrip(),z))
+        elif (re.search(pointHeader2,inputLine)):
+          fout.write(("%s %.2f 0 1\n") % (inputLine.rstrip(),z))
+        elif (re.search(pointHeader3,inputLine)):
+          fout.write(("%s %.2f 1 0\n") % (inputLine.rstrip(),z))
         # second is the ELEV header
         elif(re.search(ELEVHeader,inputLine)):
           #print(("%s found in %s") % (ELEVHeader,inputLine))
